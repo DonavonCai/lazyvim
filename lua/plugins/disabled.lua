1e519b5aa29077814646f3ac108ad9b8fc5acb9e
Donavon Cai <donavoncai1998@gmail.com>
2026-07-21 18:28:26 -0500

    Add disabled.lua

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+    { "gzip", enabled = false },
+    { "tarPlugin", enabled = false },
+    { "tohtml", enabled = false },
+    { "tutor", enabled = false },
+    { "zipPlugin", enabled = false },
+}
